55bee9c6c9205e8423e3f0370701d7daa9958785
Danila Emelchenko <emelchenko1997@yandex.ru>
2026-01-14 17:29:29 +0300

    построение структуры папок по методологии FSD

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,10 @@
 import { Route, Routes, Link } from "react-router-dom";
 import "./styles/index.scss";
-import { MainPageAsync } from "./pages/MainPage/MainPage.async";
-import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
 import { Suspense } from "react";
-import { useTheme } from "./theme/useTheme";
-import { classnames } from "./helpers/classNames/classNames";
+import { classnames } from "shared/lib/classNames";
+import { useTheme } from "./providers/ThemeProvider";
+import { MainPage } from "pages/MainPage";
+import { AboutPage } from "pages/AboutPage";
 
 export const App = () => {
   const { theme, toggleTheme } = useTheme();
@@ -15,8 +15,8 @@ export const App = () => {
       <Link to={"/about"}>О сайте</Link>
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
-          <Route path="/" element={<MainPageAsync />} />
-          <Route path="/about" element={<AboutPageAsync />} />
+          <Route path="/" element={<MainPage />} />
+          <Route path="/about" element={<AboutPage />} />
         </Routes>
       </Suspense>
     </div>
