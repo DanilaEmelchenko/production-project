96bd74907ab1e4014950f0dbca3c80301a70efe7
Danila Emelchenko <emelchenko1997@yandex.ru>
2026-02-06 15:56:40 +0300

    добавил React Testing Library для тестов на компоненты

---
+++ config/jest/jestEmptyComponents.tsx
@@ -0,0 +1,5 @@
+const jestEmptyComponents = function () {
+  return <div></div>;
+};
+
+export default jestEmptyComponents;
--- src/widgets/Sidebar/ui/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar.tsx
@@ -3,6 +3,7 @@ import { classNames } from "shared/lib/classNames/classNames";
 import cls from "./Sidebar.module.scss";
 import { ThemeSwitcher } from "shared/ui/ThemeSwitcher";
 import LangSwitcher from "shared/ui/LangSwitcher/LangSwitcher";
+import Button from "shared/ui/Button/Button";
 
 export interface SidebarProps {
   className?: string;
@@ -17,11 +18,14 @@ const Sidebar = ({ className }: SidebarProps) => {
 
   return (
     <div
+      data-testid="sidebar"
       className={classNames(cls.sidebar, { [cls.collapsed]: collapsed }, [
         className,
       ])}
     >
-      <button onClick={onToggle}>toggle</button>
+      <Button data-testid="sidebar-toggle" onClick={onToggle}>
+        toggle
+      </Button>
       <div className={cls.switchers}>
         <ThemeSwitcher />
         <LangSwitcher className={cls.lang} />
